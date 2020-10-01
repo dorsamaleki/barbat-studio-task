81789dd8907ae4be93819eeedad5c917a9ae99f9
Dorsa Maleki <a.malekidorsa@gmail.com>
2020-10-01 17:48:40 +0330

    initial categories

--- src/Categories.js
+++ src/Categories.js
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
 import { categoryList } from "./categoryList";
-
+import { v4 as uuidv4 } from "uuid";
+import styles from "./Categories.module.css";
 export const Categories = () => {
-  const category = categoryList();
-  const [list, setList] = useState(category);
+  const initialList = categoryList();
+  const [list, setList] = useState(initialList);
 
   const [name, setName] = useState();
 
@@ -11,16 +12,34 @@ export const Categories = () => {
     setName(event.target.value);
   };
   const handleAdd = () => {
-    const newList = list.concat({ name });
+    const newList = list.concat({ name, id: uuidv4() });
+    setList(newList);
+    setName("");
+  };
+  const handleRemove = (id) => {
+    const newList = list.filter((item1) => item1.id !== id);
     setList(newList);
   };
 
   return (
-    <div>
+    <div className={styles.root}>
       <div>categories</div>
       {list.map((item) => {
-        return <div key={item.id}>{item.name}</div>;
+        return (
+          <div
+            key={item.id}
+            style={{
+              textDecoration: item.isComplete ? "line-through" : "none",
+            }}
+          >
+            {item.name}
+            <button type="button" onClick={() => handleRemove(item.id)}>
+              remove
+            </button>
+          </div>
+        );
       })}
+
       <input type="text" value={name} onChange={handleChange} />
 
       <button onClick={handleAdd}>add</button>
