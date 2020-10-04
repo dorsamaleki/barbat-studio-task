4642f38113ee9553cb87535476a1bf5a404b9ddc
Dorsa Maleki <a.malekidorsa@gmail.com>
2020-10-04 10:38:24 +0330

    initial form for new product

--- src/NewProduct.js
+++ src/NewProduct.js
@@ -3,37 +3,75 @@ import { v4 as uuidv4 } from "uuid";
 import { productsList } from "./productsList";
 
 export const NewProduct = () => {
+  let newList = [{ name: "", brand: "" }];
   const initialList = productsList();
   const [name, setName] = useState();
   const [brand, setBrand] = useState();
-  const [list, setList] = useState([{ name: "" }]);
+  const [price, setPrice] = useState();
+  const [list, setList] = useState(newList);
 
-  const handleChange = (event) => {
+  const handleChangeName = (event) => {
     setName(event.target.value);
   };
   const handleChangeBrand = (event) => {
     setBrand(event.target.value);
   };
+  const handleChangePrice = (event) => {
+    setPrice(event.target.value);
+  };
   const handleSave = () => {
-    const newList = (list) => [...list, { name, id: uuidv4() }];
+    const newList = (list) => [...list, { name, brand, price, id: uuidv4() }];
     setList(newList);
     setName("");
+    setBrand("");
+    setPrice("");
     localStorage.setItem("myData", list);
+  };
+  const keyPressed = (event) => {
+    if (event.key === "Enter") {
+      handleSave();
+    }
   };
 
   return (
     <div>
       New product
       <label>name</label>
-      <input type="text" value={name} onChange={handleChange} />
+      <input
+        type="text"
+        value={name}
+        onChange={handleChangeName}
+        onKeyPress={keyPressed}
+        required
+      />
       <label>brand</label>
-      <input type="text" value={brand} onChange={handleChangeBrand} />
+      <input
+        type="text"
+        value={brand}
+        onChange={handleChangeBrand}
+        onKeyPress={keyPressed}
+        required
+      />
+      <label>price</label>
+      <input
+        type="text"
+        value={price}
+        onChange={handleChangePrice}
+        onKeyPress={keyPressed}
+        required
+      />
       <label>categories</label>
       <input type="text" />
       <button onClick={handleSave}>save</button>
       <div>
         {list.map((item) => {
-          return <div>{item.name}</div>;
+          return (
+            <div>
+              {item.name}
+              {item.brand}
+              {item.price}
+            </div>
+          );
         })}
       </div>
     </div>
